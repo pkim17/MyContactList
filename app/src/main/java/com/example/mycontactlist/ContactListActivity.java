b40b9527a356e3f0a7206276a510b6780aa73d24
Peter Kim <peterkim@Peters-MacBook-Pro-6.local>
2019-09-14 14:19:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mycontactlist/ContactListActivity.java
@@ -0,0 +1,56 @@
+package com.example.mycontactlist;
+
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+        initListButton();
+        initMapButton();
+        initSettingButton();
+    }
+
+    private void initListButton() {
+        ImageButton ibList = (ImageButton) findViewById(R.id.imageButtonList);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactListActivity.this, ContactListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initMapButton() {
+        ImageButton ibList = (ImageButton) findViewById(R.id.imageButtonMap);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactListActivity.this, ContactMapActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+    private void initSettingButton() {
+        ImageButton ibList = (ImageButton) findViewById(R.id.imageButtonSettings);
+        ibList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ContactListActivity.this, ContactSettingsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mycontactlist/DatePickerDialog.java
@@ -0,0 +1,58 @@
+package com.example.mycontactlist;
+
+import android.app.Dialog;
+import android.os.Bundle;
+//import android.support.v4.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
+import java.util.Calendar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+
+public class DatePickerDialog extends DialogFragment {
+
+    public interface SaveDateListener {
+        void didFinishDatePickerDialog(Calendar selectedTime);
+    }
+
+    public DatePickerDialog() {
+//        Empty Constructor required for DialogFragment
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.select_date,container);
+
+        getDialog().setTitle("Select Date");
+
+        final  DatePicker dp = (DatePicker) view.findViewById(R.id.datePicker);
+
+        Button saveButton = (Button) view.findViewById(R.id.buttonSelect);
+        saveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Calendar selectedTime = Calendar.getInstance();
+                selectedTime.set(dp.getYear(),dp.getMonth(),dp.getDayOfMonth());
+                saveItem(selectedTime);
+            }
+        });
+        Button cancelButton = (Button) view.findViewById(R.id.buttonCancel);
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDialog().dismiss();
+            }
+        });
+        return view;
+    }
+    private void saveItem(Calendar selectedTime) {
+        SaveDateListener activity = (SaveDateListener) getActivity();
+        activity.didFinishDatePickerDialog(selectedTime);
+        getDialog().dismiss();
+    }
+
+}
